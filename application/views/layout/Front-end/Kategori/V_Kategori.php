a2eba751e5c5cefc114590f64c64f9d36d3ed92d
ocktha <ockthadwishaputra@gmail.com>
2023-02-09 15:38:49 +0700

    Uploadan Project Kelompok

---
+++ application/views/layout/Front-end/Kategori/V_Kategori.php
@@ -0,0 +1,191 @@
+
+  <section class=" camp  ">
+    <div class="container-fluid">
+      <h1 class=" primary-title-text">Kategori</h1>
+      <p class="mb-3 secondary-title-text ">
+        Pilih Pilihan Paket dengan kategori terbaik dari kami
+      </p>
+    </div>
+  </section>
+
+
+  <div class="container mb-5 text-center ">
+
+
+    <!-- similar product -->
+    <section class="category-opentrip ">
+      <div class="row">
+        <div class="col-md-4 mt-2 col-sm-6">
+          <form action="<?= base_url('HalUtama/detailKategori')?>" method="post">
+          <button>
+          <figure class="figure">
+              <div class="hgwd-img">
+                <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/alun2.jpg" class="figure-img img-fluid">
+              </div>
+              <figcaption class="figure-caption">
+                <div class="row">
+                  <div class="col text-left">
+                    <h5 class="mt-2">Padang Safana</h5>
+                    <p>4 items</p>
+                  </div>
+                  <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+                </div>
+              </figcaption>
+          </figure>
+          <input type="hidden" name="kategori" value="Padang Safana">
+          </button>
+          </form>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/semeru.png" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">7 Tertinggi</h5>
+                  <p>5 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/pemula.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Pemula</h5>
+                  <p>2 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/curam.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Jalur Tercuram</h5>
+                  <p>3 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/landai.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Jalur Terlandai</h5>
+                  <p>3 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/pemandangan.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Pemandangan Yang Bagus</h5>
+                  <p>3 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/airterjun.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Air Terjun</h5>
+                  <p>2 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/teramai.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Jalur Teramai</h5>
+                  <p>3 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-4 mt-2 col-sm-6">
+          <figure class="figure">
+            <div class="hgwd-img">
+              <img src="<?= base_url('assets/Template/view_Front-end');?>/images/fotoGunung/tersepi.jpg" class="figure-img img-fluid">
+            </div>
+            <figcaption class="figure-caption">
+              <div class="row">
+                <div class="col text-left">
+                  <h5 class="mt-2">Jalur Tersepi</h5>
+                  <p>3 items</p>
+                </div>
+                <!-- <div class="col align-items-center d-flex justify-content-end">
+
+                </div> -->
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+
+      </div>
+    </section>
+    <!-- Akhir similar product -->
+  </div>
+  <!-- akhir container -->
+
